c75a199cebd0721141299f9b269ccff1d99fb1e7
spetterman66 <ryanbremerfromyt@outlook.com>
2024-03-28 15:17:07 +0100

    Post 2!

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # windows 8 will never die!
+a beautiful website made to look like a windows 8 metro interface. 
+even though windows 8 was hated by many, its metro design was influential and ahead of its time. it helped the world transition away from skeuomorphism and into flat design.  
+do not raise an issue about the text not being capitalized, it is intentional. 
+(mac and linux users are welcome to use this website, but it is optimized for windows vista and later, as it uses the segoe ui font.) 
+feel free to fork this repository and make use of this unique design in your own projects.
 ## screenshot
 ![screenshot](screenshot.png)
-a beautiful website made to look like a windows 8 metro interface.
-do not raise an issue about the text not being capitalized, it is intentional.
-(mac and linux users are welcome to use this website, but it is optimized for windows 8+ users, unless you can find a copy of the Segoe UI font).
+## footnotes
 this website is not affiliated with microsoft, windows, or any of their products.
 [view the website](https://spetterman66.github.io/)
--- script.js
+++ script.js
@@ -1,40 +1,39 @@
-const content = document.querySelector('.content');
-content.addEventListener('wheel', (e) => {
-    e.preventDefault();
-    content.scrollLeft += e.deltaY;
+var content = document.querySelector('.content');
+content.addEventListener('wheel', function (e) {
+  e.preventDefault();
+  content.scrollLeft += e.deltaY;
 });
 function initialize() {
-    // fetch current theme from localstorage
-    const theme = localStorage.getItem('theme');
-    const settingstext = document.getElementById('settingstext');
-    const contacttext = document.getElementById('contacttext');
-    const contactcontent = document.getElementById('contact-content');
-    const settingscontent = document.getElementById('settings-content');
-    const share = document.getElementById('share');
-    const help = document.getElementById('help');
-    const hometext = document.getElementById('hometext');
-    const section = document.querySelectorAll('section');
-    const select = document.querySelectorAll('select');
-    const close = document.querySelectorAll('#close');
-    // if theme is dark then set dark theme
-    if (theme === 'light') {
-        document.body.classList.add('light');
-        settingstext.classList.add('light');
-        contacttext.classList.add('light');
-        contactcontent.classList.add('light');
-        settingscontent.classList.add('light');
-        share.classList.add('light');
-        help.classList.add('light');
-        hometext.classList.add('light');
-        section.forEach((element) => {
-            element.classList.add('light');
-        });
-        select.forEach((element) => {
-            element.classList.add('light');
-        });
-        close.forEach((element) => {
-            element.classList.add('light');
-        });
-    }
-    else return;
+  // fetch current theme from localstorage
+  var theme = localStorage.getItem('theme');
+  var settingstext = document.getElementById('settingstext');
+  var contacttext = document.getElementById('contacttext');
+  var contactcontent = document.getElementById('contact-content');
+  var settingscontent = document.getElementById('settings-content');
+  var share = document.getElementById('share');
+  var help = document.getElementById('help');
+  var hometext = document.getElementById('hometext');
+  var section = document.querySelectorAll('section');
+  var select = document.querySelectorAll('select');
+  var close = document.querySelectorAll('#close');
+  // if theme is dark then set dark theme
+  if (theme === 'light') {
+    document.body.classList.add('light');
+    settingstext.classList.add('light');
+    contacttext.classList.add('light');
+    contactcontent.classList.add('light');
+    settingscontent.classList.add('light');
+    share.classList.add('light');
+    help.classList.add('light');
+    hometext.classList.add('light');
+    section.forEach(function (element) {
+      element.classList.add('light');
+    });
+    select.forEach(function (element) {
+      element.classList.add('light');
+    });
+    close.forEach(function (element) {
+      element.classList.add('light');
+    });
+  } else return;
 }
